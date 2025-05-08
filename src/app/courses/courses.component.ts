326e0d1003739c65579c12eaa416465556c972f7
Tornrosen <elec11anna@hotmail.com>
2025-05-08 12:08:33 +0200

    skapa filstruktur, httpclient, visa kurser

---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { CourseService } from '../services/course.service';
+
+@Component({
+  selector: 'app-courses',
+  imports: [CommonModule],
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.css'
+})
+export class CoursesComponent {
+ courselist: Course[]=[];
+
+ constructor(private courseservice: CourseService){}
+
+ ngOnInit() {
+  this.courseservice.getCourses().subscribe(data => {
+    this.courselist=data;
+  })
+ }
+}
---
+++ src/app/services/course.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course} from '../model/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+  private url: string ="ramschema.json";
+  constructor(private http: HttpClient) { }
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.url);
+  }
+}
